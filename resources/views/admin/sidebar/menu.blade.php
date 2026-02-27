812585ed07b4f0defae43621f35bc4230e464452
misti <idlkz2019@gmail.com>
2026-02-27 10:58:05 +0500

    judge-cities

--- resources/views/admin/sidebar/menu.blade.php
+++ resources/views/admin/sidebar/menu.blade.php
@@ -10,162 +10,214 @@
 
 @include('shared.components.sidebar_section', ['title' => __('ui.management')])
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::ROLES_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.roles',
     'label' => __('ui.roles'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::PERMISSIONS_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.permissions',
     'label' => __('ui.permissions'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::ROLE_PERMISSIONS_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.role-permissions',
     'label' => __('crud.role_permissions'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::USERS_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.users',
     'label' => __('ui.users'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"/></svg>',
 ])
+@endif
 
 @include('shared.components.sidebar_section', ['title' => __('ui.references')])
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::COUNTRIES_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.countries',
     'label' => __('ui.countries'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::CITIES_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.cities',
     'label' => __('ui.cities'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::JUDGE_TYPES_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.judge-types',
     'label' => __('ui.judge_types'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::JUDGE_CITIES_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.judge-cities',
     'label' => __('ui.judge_cities'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::JUDGE_REQUIREMENTS_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.judge-requirements',
     'label' => __('ui.judge_requirements'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::TOURNAMENTS_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.tournaments',
     'label' => __('ui.tournaments'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::SEASONS_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.seasons',
     'label' => __('ui.seasons'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::CLUB_TYPES_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.club-types',
     'label' => __('ui.club_types'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::CLUBS_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.clubs',
     'label' => __('ui.clubs'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::CLUB_STADIUMS_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.club-stadiums',
     'label' => __('crud.club_stadiums'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::STADIUMS_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.stadiums',
     'label' => __('ui.stadiums'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::HOTELS_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.hotels',
     'label' => __('crud.hotels'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::HOTEL_ROOMS_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.hotel-rooms',
     'label' => __('crud.hotel_rooms'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::FACILITIES_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.facilities',
     'label' => __('ui.facilities'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::ROOM_FACILITIES_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.room-facilities',
     'label' => __('ui.room_facilities'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::TRANSPORT_TYPES_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.transport-types',
     'label' => __('ui.transport_types'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"/></svg>',
 ])
+@endif
 
 @include('shared.components.sidebar_section', ['title' => __('ui.operations')])
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::CATEGORY_OPERATIONS_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.category-operations',
     'label' => __('ui.category_operations'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::OPERATIONS_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.operations',
     'label' => __('ui.operations'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::ROLE_OPERATIONS_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.role-operations',
     'label' => __('crud.role_operations'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::MATCHES_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.matches',
     'label' => __('ui.matches'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::MATCH_LOGISTS_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.match-logists',
     'label' => __('crud.match_logists'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z"/></svg>',
 ])
+@endif
 
+@if(auth()->user()->hasPermission(\App\Constants\PermissionConstants::MATCH_PROTOCOL_REQUIREMENTS_INDEX))
 @include('shared.components.sidebar_item', [
     'route' => 'admin.match-protocol-requirements',
     'label' => __('crud.match_protocol_requirements'),
     'icon'  => '<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/></svg>',
 ])
+@endif
